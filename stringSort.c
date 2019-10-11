a24c594f12f708b6803982b077d637d668f57b14
GaelB505 <33232734+GaelB505@users.noreply.github.com>
2019-10-11 10:47:15 -0700

    sort string

---
+++ stringSort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int stringSort(char*, char*);
+
+int stringSort(char *first, char *second) {
+    /*if first comes before second, return 0*/
+    /*if second should come first, return 1*/
+    char *traverse1;
+    char *traverse2;
+    
+    char letter1;
+    char letter2;
+
+    traverse1 = first;
+    traverse2 = second;
+
+    letter1 = *traverse1;
+    letter2 = *traverse2;
+
+    while ((letter1 != '\0') && (letter2 != '\0')) {
+        if (letter1 < letter2) {
+            return 0;
+        } else if (letter2 < letter1) {
+            return 1;
+        } else {
+            traverse1++;
+            traverse2++;
+            letter1 = *traverse1;
+            letter2 = *traverse2;
+        }
+
+    }
+
+}
